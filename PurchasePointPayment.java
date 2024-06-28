6eac73342945cbcd7d7c696055857c36d4c071c3
onzren <107232052+onzren@users.noreply.github.com>
2024-06-28 12:12:48 +0900

    Update PurchasePointPayment.java

--- PurchasePointPayment.java
+++ PurchasePointPayment.java
@@ -5,24 +5,23 @@ class PurchasePointPayment {
     final PurchasePoint consumptionPoint; // 購入で消費するポイント
     final LocalDateTime paymentDateTime; // 購入日時
     PurchasePointPayment(final Customer customer, final Comic comic) {
-        if (customer.isEnabled()) {
-            customerId = customer.id;
-            if (comic.isEnabled()) {
+        if (!customer.isEnabled()) {
+            throw new IllegalArgumentException("有効な購入者ではありません。");
+        }
+        customerId = customer.id;
+        if (comic.isEnabled()) {
             comicId = comic.id;
-                if (comic.currentPurchasePoint.amount <= customer.possessionPoint.amount) {
-                    consumptionPoint = comic.currentPurchasePoint;
-                    paymentDateTime = LocalDateTime.now();
-                }
-                else {
-                    throw new RuntimeException("所持ポイントが不足しています。");
-                }
+            if (comic.currentPurchasePoint.amount <= customer.possessionPoint.amount) {
+                consumptionPoint = comic.currentPurchasePoint;
+                paymentDateTime = LocalDateTime.now();
             }
             else {
-                throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
+                throw new RuntimeException("所持ポイントが不足しています。");
             }
         }
         else {
-            throw new IllegalArgumentException("有効な購入者ではありません。");
+            throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
         }
-    }
+    }        
 }
+
